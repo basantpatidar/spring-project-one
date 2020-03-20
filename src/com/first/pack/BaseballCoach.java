8b2539bef8fb60973f288ee317f405238ada8f5c
basantpatidar <basantpatidar33@gmail.com>
2020-03-19 22:33:30 -0400

    class and interface

--- src/com/first/pack/BaseballCoach.java
+++ src/com/first/pack/BaseballCoach.java
@@ -2,8 +2,24 @@ package com.first.pack;
 
 public class BaseballCoach implements Coach {
 
+	//define a private field for the dependency
+	private FortuneService fortuneService;
+	
+	//define a constructor for dependency injection
+	public BaseballCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Get Set Go for 30 Minutes";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		//use my fortuneService to get a fortune		
+		return fortuneService.getFortune();
 	}
 }
